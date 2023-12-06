924484afaf8e3f0532018854dd62d61d838808b2
Hauke Wernecke <hower@kth.se>
2023-12-06 16:03:02 +0100

    Add files for configs. and a sample spike train.

---
+++ params/config_handler.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Summary:
+
+Description:
+
+
+"""
+#===============================================================================
+# PROGRAM METADATA
+#===============================================================================
+__author__ = 'Hauke Wernecke'
+__contact__ = 'hower@kth.se'
+__version__ = '0.1'
+
+
+__all__ = [
+    "config"
+]
+#===============================================================================
+# IMPORT STATEMENTS
+#===============================================================================
+
+from params import BaseConfig
+from params import BrianConfig, GateConfig, SelectConfig, GateRepeatConfig, RandomLocationConfig
+
+
+### Set the current config for all scripts/analyses here:
+config = RandomLocationConfig()
